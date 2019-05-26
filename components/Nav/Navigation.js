68076156d8525ed71556dce404aea913a36be477
PascalUlor <pascalulor@yahoo.com>
2019-05-26 22:27:34 +0100

    ft(create nav component)

--- components/Nav/Navigation.js
+++ components/Nav/Navigation.js
@@ -33,3 +33,41 @@ const gallery = document.querySelectorAll('div[class*="card"]');
 gallery.forEach((card) => {
   TweenMax.from(card, 2, { x: 100, ease: Bounce.easeOut });
 });
+
+const navBar = document.querySelector('.nav__bar');
+const NavData = [
+  {
+    logo: 'Wedding Planner',
+    links: [{ Home: 'index.html' }, { Signup: 'https://weddingplanner.netlify.com/register' }, { Login: 'https://weddingplanner.netlify.com/login' }, { AboutUs: 'about.html' }],
+  },
+];
+
+class NavBar {
+  constructor(navbar, data) {
+    this.nav = navbar;
+    this.data = data;
+    this.attachLogo();
+    this.attachNavItems();
+  }
+
+  attachLogo() {
+    const logo = document.createElement('h2');
+    logo.textContent = this.data.logo;
+    logo.classList.add('logo');
+    this.nav.append(logo);
+  }
+
+  attachNavItems() {
+    const navItemContainer = document.createElement('div');
+    navItemContainer.classList.add('nav__item');
+    this.nav.insertAdjacentElement('beforeend', navItemContainer);
+    this.data.links.forEach((link) => {
+      const navItems = document.createElement('a');
+      navItems.textContent = Object.keys(link);
+      navItems.href = Object.values(link);
+      navItemContainer.insertAdjacentElement('beforeend', navItems);
+    });
+  }
+}
+
+new NavBar(navBar, NavData[0]);
